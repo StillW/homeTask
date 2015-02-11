9a7de668a8b49231a209105a6a6193bda30141aa
Still <brizbosfor@gmail.com>
2015-02-11 15:27:09 +0300

    •	Создать класс, который содержит int и char (не проинициализированные) и распечатать их значения.
    Решение этой задачи

---
+++ src/com/sourceit/homework/hw01/A.java
@@ -0,0 +1,17 @@
+package com.sourceit.homework.hw01;
+
+/**
+ * Created by Still on 11.02.2015.
+ */
+public class A {
+
+    int intValue;
+    char charValue;
+
+
+    public void printValues() {
+
+        System.out.println("Int value: " + intValue);
+        System.out.println("Char value: " + charValue);
+    }
+}
