5419c4768e845078540ae17edd8c0bed25ea6870
Still <brizbosfor@gmail.com>
2015-02-13 01:18:43 +0300

    Домашнее задание №2

--- src/com/sourceit/homework/hw01/Main.java
+++ src/com/sourceit/homework/hw01/Main.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.util.Date;
 
 /**
+ * 	Создать класс Dog, который будет содержать 2 поля name и says.
+ * В main( ) создать 2 экземпляра класса с name “spot” (says, “Ruff!”) и “scruffy” (says, “Wurf!”). И распечатать поля объектов.
  * Created by Still on 10.02.2015.
  */
 public class Main {
