bd87f3e194122976d710a0685c1f8c323157dc7d
Still <brizbosfor@gmail.com>
2015-02-13 01:11:34 +0300

    Домашнее задание задача номер 1. Условия всех задач я записал в коментах кода.

--- src/com/sourceit/homework/hw01/A.java
+++ src/com/sourceit/homework/hw01/A.java
@@ -1,6 +1,7 @@
 package com.sourceit.homework.hw01;
 
 /**
+ * Создать класс, который содержит int и char (не проинициализированные) и распечатать их значения.
  * Created by Still on 11.02.2015.
  */
 public class A {
