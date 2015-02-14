122342434690beb5303e8f19d22d4425fe5a593b
Still <brizbosfor@gmail.com>
2015-02-14 15:39:23 +0300

    Thats is the second task on the subject of cycles.

---
+++ src/com/sourceit/homework/hw03/Fibona.java
@@ -0,0 +1,21 @@
+package com.sourceit.homework.hw03;
+
+/**
+ * •	Выведите на экран первые 11 членов последовательности Фибоначчи.
+ * Напоминаем, что первый и второй члены последовательности равны единицам,
+ * а каждый следующий — сумме двух предыдущих.
+ * Created by Still on 14.02.2015.
+ */
+public class Fibona {
+    public static void main(String[] args) {
+        int a=1,b=1,c;
+        System.out.println("Thats first two numbers of sequence " +a+ " and "+b);
+
+        for (int q=3;q<11;q++){
+            c = a+b;
+            a=b;
+            b=c;
+            System.out.println("next members of the sequence " +c+ " ");
+        }
+    }
+}
